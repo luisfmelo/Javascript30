fb1358411326032f42689d137094e53083e6bc07
Luis Melo <luismelo7@gmail.com>
2017-01-06 16:35:31 +0000

    finish day 4

--- 04_ArrayCardioDay1/script.js
+++ 04_ArrayCardioDay1/script.js
@@ -3,20 +3,67 @@
 
 // Some data we can work with
 
-const inventors = [
-  { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
-  { first: 'Isaac', last: 'Newton', year: 1643, passed: 1727 },
-  { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
-  { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
-  { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1630 },
-  { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
-  { first: 'Max', last: 'Planck', year: 1858, passed: 1947 },
-  { first: 'Katherine', last: 'Blodgett', year: 1898, passed: 1979 },
-  { first: 'Ada', last: 'Lovelace', year: 1815, passed: 1852 },
-  { first: 'Sarah E.', last: 'Goode', year: 1855, passed: 1905 },
-  { first: 'Lise', last: 'Meitner', year: 1878, passed: 1968 },
-  { first: 'Hanna', last: 'Hammarström', year: 1829, passed: 1909 }
-];
+const inventors = [{
+    first: 'Albert',
+    last: 'Einstein',
+    year: 1879,
+    passed: 1955
+}, {
+    first: 'Isaac',
+    last: 'Newton',
+    year: 1643,
+    passed: 1727
+}, {
+    first: 'Galileo',
+    last: 'Galilei',
+    year: 1564,
+    passed: 1642
+}, {
+    first: 'Marie',
+    last: 'Curie',
+    year: 1867,
+    passed: 1934
+}, {
+    first: 'Johannes',
+    last: 'Kepler',
+    year: 1571,
+    passed: 1630
+}, {
+    first: 'Nicolaus',
+    last: 'Copernicus',
+    year: 1473,
+    passed: 1543
+}, {
+    first: 'Max',
+    last: 'Planck',
+    year: 1858,
+    passed: 1947
+}, {
+    first: 'Katherine',
+    last: 'Blodgett',
+    year: 1898,
+    passed: 1979
+}, {
+    first: 'Ada',
+    last: 'Lovelace',
+    year: 1815,
+    passed: 1852
+}, {
+    first: 'Sarah E.',
+    last: 'Goode',
+    year: 1855,
+    passed: 1905
+}, {
+    first: 'Lise',
+    last: 'Meitner',
+    year: 1878,
+    passed: 1968
+}, {
+    first: 'Hanna',
+    last: 'Hammarström',
+    year: 1829,
+    passed: 1909
+}];
 
 const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Begin, Menachem', 'Belloc, Hilaire', 'Bellow, Saul', 'Benchley, Robert', 'Benenson, Peter', 'Ben-Gurion, David', 'Benjamin, Walter', 'Benn, Tony', 'Bennington, Chester', 'Benson, Leana', 'Bent, Silas', 'Bentsen, Lloyd', 'Berger, Ric', 'Bergman, Ingmar', 'Berio, Luciano', 'Berle, Milton', 'Berlin, Irving', 'Berne, Eric', 'Bernhard, Sandra', 'Berra, Yogi', 'Berry, Halle', 'Berry, Wendell', 'Bethea, Erin', 'Bevan, Aneurin', 'Bevel, Ken', 'Biden, Joseph', 'Bierce, Ambrose', 'Biko, Steve', 'Billings, Josh', 'Biondo, Frank', 'Birrell, Augustine', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
 
@@ -24,11 +71,11 @@ const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick
 // 1. Filter the list of inventors for those who were born in the 1500's
 // we pass a function (like for Each inventor in inventors)
 // for each inventor, if the function return true... it will keep him
-  /*
-    const fifteen = inventors.filter(function (inventor){
-      return (inventor.year >= 1500 && inventor.year < 1600);
-    });
-  */
+/*
+  const fifteen = inventors.filter(function (inventor){
+    return (inventor.year >= 1500 && inventor.year < 1600);
+  });
+*/
 //ES6
 const fifteen = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600)
 console.table(fifteen); // pro tip: display Object as Table
@@ -39,13 +86,13 @@ console.table(fifteen); // pro tip: display Object as Table
 
 // Array.prototype.map()
 // 2. Give us an array of the inventors' first and last
-  /*
-    const firstANDlast = inventors.map(function(inventor){
-      return inventor.first + ' ' + inventor.last;
-    });
-  */
+/*
+  const firstANDlast = inventors.map(function(inventor){
+    return inventor.first + ' ' + inventor.last;
+  });
+*/
 const firstANDlast = inventors.map(inventor => `${inventor.first} ${inventor.last}`)
-console.table(firstANDlast);
+console.log(firstANDlast);
 
 /*****************************************************************************/
 
@@ -55,11 +102,11 @@ console.table(firstANDlast);
 // Array.prototype.sort()
 // 3. Sort the inventors by birthdate, oldest to youngest
 // a and b are two consecutive persons. Then I return the sort rule
-  /*
-    const sortedByBirthday = inventors.sort(function(a,b){
-      return a.year > b.year ? 1 : -1;
-    });
-    */
+/*
+  const sortedByBirthday = inventors.sort(function(a,b){
+    return a.year > b.year ? 1 : -1;
+  });
+*/
 const sortedByBirthday = inventors.sort((a, b) => a.year > b.year ? 1 : -1)
 console.table(sortedByBirthday);
 
@@ -72,9 +119,9 @@ console.table(sortedByBirthday);
 // Array.prototype.reduce()
 // 4. How many years did all the inventors live?
 // it's like a map but it only return 1 value (it is about all the inventor)
-// 1st variable is the common one... the second is an element of array 
-const totalYears = inventors.reduce(function(total, inventor){
-  return total + (inventor.passed - inventor.year);
+// 1st variable is the common one... the second is an element of array
+const totalYears = inventors.reduce(function(total, inventor) {
+    return total + (inventor.passed - inventor.year);
 }, 0);
 
 console.log(totalYears);
@@ -86,14 +133,66 @@ console.log(totalYears);
 
 
 // 5. Sort the inventors by years lived
+/*
+const sortByYearsLived = inventors.sort(function(a,b){
+  const ylivedByA = a.passed-a.year;
+  const ylivedByB = b.passed-b.year;
+  return ylivedByA < ylivedByB ? 1 : -1;
+});
+*/
+
+const sortByYearsLived = inventors.sort((a, b) => (a.passed - a.year) < (b.passed - b.year) ? 1 : -1)
+console.table(sortByYearsLived);
+
+/*****************************************************************************/
+
+
+
+
+
+
 
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
+/* const category = document.querySelector('.mw-category');
+ // This is called a spread. The inner content is a NodeList. With a Spread,
+ // I take all the elements of that list and in above the array
+ const links = [...category.querySelectorAll('a')];
+
+ const de = links
+                .map(link => link.textContent)
+                .filter(text => text.includes('de'));
+
+*****************************************************************************/
+
+
+
 
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+const sortAlpha = people.sort((lastOne, nextOne) => {
+    const [aLast, aFirst] = lastOne.split(', ');
+    const [bLast, bFirst] = nextOne.split(', ');
+    return aLast > bLast ? 1 : -1;
+})
+console.log(sortAlpha)
+
+/*****************************************************************************/
+
+
+
+
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
-const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck' ];
+const data = ['car', 'car', 'truck', 'truck', 'bike', 'walk', 'car', 'van', 'bike', 'walk', 'car', 'van', 'car', 'truck'];
+
+const transportation = data.reduce(function(obj, item) {
+    if (!obj[item])
+        obj[item] = 0;
+    obj[item]++;
+    return obj;
+}, {});
+
+console.log(transportation)
