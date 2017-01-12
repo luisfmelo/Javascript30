562963ebf665400979edf0e736937ccf2bda07b8
luisfmelo <luismelo7@gmail.com>
2017-01-12 09:46:28 +0000

    finish day 12

---
+++ 12_KeySequenceDetection/script.js
@@ -0,0 +1,9 @@
+const pressed = [];
+const secretCode = 'melo';
+window.addEventListener('keyup', (e) => {
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode))
+    cornify_add();
+  console.log(pressed)
+});
