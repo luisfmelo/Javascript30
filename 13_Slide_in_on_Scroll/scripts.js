e2effa59d3eea4f0586562ad8bd3cf7792a8fd20
Luís Melo <luismelo7@gmail.com>
2017-01-29 14:57:57 +0000

    finish day 13

---
+++ 13_Slide_in_on_Scroll/scripts.js
@@ -0,0 +1,37 @@
+const sliderImages = document.querySelectorAll('.slide-in');
+
+window.addEventListener('scroll', debounce(checkslide));
+
+function checkslide(e){
+  sliderImages.forEach(image => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - image.height/2;
+    // bottom of the image
+    const imageBottom = image.offsetTop + image.height;
+
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast)
+      image.classList.add('active');
+    else
+      image.classList.remove('active');
+
+  });
+}
+
+/* Run func only ever 20ms */
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
